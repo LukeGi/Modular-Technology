ee0fd7305db00df7604b7b4cbda924185cc1c5e2
bluemonster122 <boo122333@gmail.com>
2016-02-11 20:35:34 +0000

    Particle effect polished and renamed to something more suitable

--- src/main/java/net/blep/modtech/items/item/ItemSpawnParticle.java
+++ src/main/java/net/blep/modtech/items/item/ItemSpawnParticle.java
@@ -23,7 +23,7 @@ public class ItemSpawnParticle extends ModItem {
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
         if (world.isRemote) {
             ForgeDirection d = ForgeDirection.getOrientation(side);
-            ParticleManager.spawnParticlez(MessageSpawnParticle.ParticleTypes.SHINYLIGHTNING, world, new Vector3d(x + d.offsetX + 0.5, y + d.offsetY + 0.5, z + d.offsetZ + 0.5), 10);
+            ParticleManager.spawnParticlez(MessageSpawnParticle.ParticleTypes.SHINYLIGHTNING, world, new Vector3d(x + d.offsetX + 0.5, y + d.offsetY + 0.5, z + d.offsetZ + 0.5), 2);
         }
         return super.onItemUse(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
     }
