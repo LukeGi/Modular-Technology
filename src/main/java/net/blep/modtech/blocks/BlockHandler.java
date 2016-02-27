4b72d67cca41b54414732917e58d489702094bb4
bluemonster122 <boo122333@gmail.com>
2016-02-27 22:32:04 +0000

    Did some stuff with treefarm

--- src/main/java/net/blep/modtech/blocks/BlockHandler.java
+++ src/main/java/net/blep/modtech/blocks/BlockHandler.java
@@ -2,16 +2,12 @@ package net.blep.modtech.blocks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.blep.modtech.blocks.block.*;
-import net.blep.modtech.blocks.tileentity.TileEntityMachine;
 import net.blep.modtech.blocks.tileentity.TileEntityTreeFarm;
-import net.blep.modtech.blocks.tileentity.machines.TileEntityFurnace;
-import net.blep.modtech.core.ModTech;
 import net.blep.modtech.core.reference.ModInfo;
 import net.blep.modtech.core.util.EnumMachineProperties;
 import net.blep.modtech.items.itemblock.ItemBlockMachine;
 import net.blep.modtech.items.itemblock.ItemBlockMetalBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockRail;
 
 /**
  * Created by Kelan on 24/01/2016.
@@ -33,16 +29,13 @@ public class BlockHandler
         GameRegistry.registerBlock(BLOCK_METAL_ORE, ItemBlockMetalBlock.class, BLOCK_PREFIX + "ore");
         GameRegistry.registerBlock(BLOCK_METAL_BLOCK, ItemBlockMetalBlock.class, BLOCK_PREFIX + "metal");
         GameRegistry.registerBlock(BLOCK_MACHINE, ItemBlockMachine.class, BLOCK_PREFIX + "machine");
+        for (int i = 0; i < EnumMachineProperties.values().length; i++)
+            GameRegistry.registerTileEntity(EnumMachineProperties.values()[i].getTileClass(), TILEENTITY_PREFIX + EnumMachineProperties.values()[i].getName().toLowerCase());
         GameRegistry.registerBlock(BLOCK_TEST_SIDES, BLOCK_PREFIX + "test");
         GameRegistry.registerBlock(TEST, "blockTest");
+        GameRegistry.registerTileEntity(BlockMadness.TileEntityParticleGenerator.class, ModInfo.RESOURCE_PREFIX + "particleGenerator");
         GameRegistry.registerBlock(TREE_FARM, "treeFarm");
         GameRegistry.registerTileEntity(TileEntityTreeFarm.class, ModInfo.RESOURCE_PREFIX + "treeFarm");
-        GameRegistry.registerTileEntity(BlockMadness.TileEntityParticleGenerator.class, ModInfo.RESOURCE_PREFIX + "particleGenerator");
 
-        for (int i = 0; i < EnumMachineProperties.values().length; i++)
-        {
-            EnumMachineProperties machine = EnumMachineProperties.values()[i];
-            GameRegistry.registerTileEntity(machine.getTileClass(), TILEENTITY_PREFIX + machine.getName().toLowerCase());
-        }
     }
 }
