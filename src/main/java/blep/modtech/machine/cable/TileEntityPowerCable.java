6bc204a57c19dc9efe973f9453d1529df742d845
Blue_Monster122 <boo122333@gmail.com>
2016-04-17 14:43:49 +0100

    Sucessfully adds cables

--- src/main/java/blep/modtech/machine/cable/TileEntityPowerCable.java
+++ src/main/java/blep/modtech/machine/cable/TileEntityPowerCable.java
@@ -1,28 +1,18 @@
 package blep.modtech.machine.cable;
 
-import blep.modtech.util.LogHelper;
 import cofh.api.energy.EnergyStorage;
 import cofh.api.energy.IEnergyProvider;
 import cofh.api.energy.IEnergyReceiver;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.items.ItemStackHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
 
 /**
  * Created by Blue <boo122333@gmail.com>.
  */
 public class TileEntityPowerCable extends TileEntity implements IEnergyProvider, IEnergyReceiver, ITickable
 {
-    List<BlockPos> children;
-    BlockPos master;
-    private boolean isMaster, firstrun = true;
     private EnergyStorage energyStorage;
 
     public TileEntityPowerCable(int powerTransfer)
@@ -33,69 +23,37 @@ public class TileEntityPowerCable extends TileEntity implements IEnergyProvider,
     @Override
     public void update()
     {
-        if (firstrun)
+        for (EnumFacing dir : EnumFacing.VALUES)
         {
-            firstrun = false;
-            initialise();
-        }
-        for (EnumFacing facing : EnumFacing.VALUES)
-        {
-            TileEntity tile = worldObj.getTileEntity(pos.add(facing.getDirectionVec()));
-            if (tile instanceof IEnergyProvider)
-                energyStorage.modifyEnergyStored(((IEnergyProvider) tile).extractEnergy(facing.getOpposite(), energyStorage.getMaxEnergyStored() - energyStorage.getEnergyStored(), false));
-            if (tile instanceof IEnergyReceiver && !(tile instanceof TileEntityPowerCable))
-                energyStorage.modifyEnergyStored(-((IEnergyReceiver) tile).receiveEnergy(facing.getOpposite(), energyStorage.getEnergyStored(), false));
+            BlockPos searchPos = pos.add(dir.getDirectionVec());
+            TileEntity tileNextdoor = worldObj.getTileEntity(searchPos);
+            if (tileNextdoor instanceof TileEntityPowerCable)
+                balanceWith((TileEntityPowerCable)tileNextdoor, dir);
+            else if (tileNextdoor instanceof IEnergyProvider)
+                takePower((IEnergyProvider)tileNextdoor, dir);
+            else if (tileNextdoor instanceof IEnergyReceiver)
+                givePower((IEnergyReceiver)tileNextdoor, dir);
         }
     }
 
-    private void initialise()
+    private void givePower(IEnergyReceiver tileNextdoor, EnumFacing dir)
     {
-        if (master == null)
-        {
-            List<TileEntityPowerCable> connectedStorages = new ArrayList<>();
-            Stack<TileEntityPowerCable> traversingStorages = new Stack<>();
-            TileEntityPowerCable master = this;
-            traversingStorages.add(this);
-            connectedStorages.add(this);
-            while (!traversingStorages.isEmpty())
-            {
-                TileEntityPowerCable storage = traversingStorages.pop();
-                if (storage.isMaster())
-                {
-                    master = storage;
-                }
-                for (EnumFacing b : EnumFacing.values())
-                {
-                    TileEntity te = worldObj.getTileEntity(new BlockPos(storage.pos).add(b.getDirectionVec()));
-                    if (te instanceof TileEntityPowerCable && !connectedStorages.contains(te))
-                    {
-                        connectedStorages.add((TileEntityPowerCable) te);
-                        traversingStorages.add((TileEntityPowerCable) te);
-                    }
-                }
-            }
-            LogHelper.info("Setting master to " + master.pos.toString().substring(8) + " for " + connectedStorages.size() + " blocks");
-            for (TileEntityPowerCable storage : connectedStorages)
-            {
-                storage.setMaster(master, connectedStorages.size());
-            }
-        }
-
-        for (EnumFacing direction : EnumFacing.VALUES)
-        {
-            TileEntity tile = worldObj.getTileEntity(pos.add(direction.getDirectionVec()));
-            if (tile instanceof TileEntityPowerCable){
-                if (((TileEntityPowerCable) tile).isMaster())
-                    master = pos.add(direction.getDirectionVec());
-                else master = ((TileEntityPowerCable) tile).getMaster();
-            }
-        }
+        energyStorage.modifyEnergyStored(-tileNextdoor.receiveEnergy(dir.getOpposite(), energyStorage.getEnergyStored(), false));
     }
 
-    private void setMaster(TileEntityPowerCable master, int size)
+    private void takePower(IEnergyProvider tileNextdoor, EnumFacing dir)
     {
-        this.master = master.getPos();
-        isMaster = master == this;
+        energyStorage.modifyEnergyStored(tileNextdoor.extractEnergy(dir.getOpposite(), energyStorage.getMaxEnergyStored() - energyStorage.getEnergyStored(), false));
+    }
+
+    private void balanceWith(TileEntityPowerCable that, EnumFacing dir)
+    {
+        int thisEnergy = getEnergyStored(dir);
+        int thatEnergy = that.getEnergyStored(dir.getOpposite());
+        if (thisEnergy < thatEnergy)
+            energyStorage.modifyEnergyStored(-that.receiveEnergy(dir.getOpposite(), (thisEnergy - thatEnergy) / 2, false));
+        if (thatEnergy < thisEnergy)
+            that.balanceWith(this, dir.getOpposite());
     }
 
     @Override
@@ -126,15 +84,5 @@ public class TileEntityPowerCable extends TileEntity implements IEnergyProvider,
     public boolean canConnectEnergy(EnumFacing from)
     {
         return true;
-    }
-
-    public boolean isMaster()
-    {
-        return isMaster;
-    }
-
-    public BlockPos getMaster()
-    {
-        return master;
     }
 }
